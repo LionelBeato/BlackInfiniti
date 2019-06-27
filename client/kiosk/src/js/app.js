bae5fa5821473daf6f1f542063be6b938063063f
Jordan Mor <jordanmor@gmail.com>
2019-06-27 15:35:20 -0400

    Refactor

--- client/kiosk/src/js/app.js
+++ client/kiosk/src/js/app.js
@@ -2,6 +2,7 @@
             Slider
    ====-====-====-====-==== */
 
+const $flipsterUL = $('.my-flipster ul');
 const modelsArr = ['Q50', 'Q70', 'QX30', 'QX50', 'QX80'];
 const modelColors = {
   Q50: ['Majestic White', 'Hermosa Blue', 'Liquid Platinum', 'Black Obsidian', 'Graphite Shadow'],
@@ -13,45 +14,44 @@ const modelColors = {
 
 init();
 
-function init() {
-  populateModelSlider();
-  activateFlipster();
-}
-
-function populateModelSlider() {
-  const $flipsterUL = $('.my-flipster ul');
-  for(let i = 0; i < modelsArr.length; i++) {
-    $flipsterUL.append(`<li><a><img src="img/model-slider/${modelsArr[i]}.jpg"/></a><h2>${modelsArr[i]}</h2></li>`)
-  }
-}
-
-$('.my-flipster a').on('click', e => {
+$('.my-flipster img').on('click', e => {
   e.preventDefault();
-  if(e.target.src.includes('model-slider')) {
-    // const model = e.target.src.match(/.+\/(.+)\.jpg$/);
-    const model = e.currentTarget.nextElementSibling.textContent;
-    console.log(model);
-    populateColorSlider(model);
-    const myFlipster = activateFlipster();
-    myFlipster.flipster('index');
-    const middleIndex = findMiddleIndex(model);
-    myFlipster.flipster('jump', middleIndex);
-  } else if(e.target.src.includes('color-slider')) {
-
+  const target = e.target;
+  if(target.src.includes('model-slider')) {
+    // Grab model name from h2 content inside selected flipster li 
+    const model = target.nextElementSibling.textContent;
+    populateSlider('color', model);
+    resetFlipster(model);
+  } else if(target.src.includes('color-slider')) {
+      // display results page
   }
 });
 
-function populateColorSlider(model) {
-  const imagesArr = modelColors[model];
-  let html = '';
-  for(let i = 0; i < imagesArr.length; i++) {
-    html += `<li><a><img src="img/color-slider/${i + 1}.jpeg"/></a><h2>${model} - ${imagesArr[i]}</h2></li>`
-  }
-  $('.my-flipster ul').html(html);
+function init() {
+  populateSlider('model');
+  activateFlipster();
 }
 
-function findMiddleIndex(model) {
-  const indices = modelColors[model].length;
-  return Math.floor(indices / 2);
+// Valid type parameters = 'model', color'
+function populateSlider(type, model) {
+  if(type == 'model') {
+    for(let model of modelsArr) {
+      $flipsterUL.append(`<li><img src="img/model-slider/${model}.jpg"/><h2>${model}</h2></li>`);
+    }
+  } else if(type = 'color') {
+      const imagesArr = modelColors[model];
+      $('.my-flipster ul').empty();
+      for(let i = 0; i < imagesArr.length; i++) {
+        $('.my-flipster ul').append(`<li><img src="img/color-slider/${model}/${i + 1}.jpeg"/><h2>${model} - ${imagesArr[i]}</h2></li>`);
+      }
+  }
+}
+
+function resetFlipster(model) {
+  const myFlipster = activateFlipster();
+  myFlipster.flipster('index');
+  const middleIndex = Math.floor(modelColors[model].length / 2);
+  // Reset flipster so it jumps to the middle index
+  myFlipster.flipster('jump', middleIndex);
 }
 
