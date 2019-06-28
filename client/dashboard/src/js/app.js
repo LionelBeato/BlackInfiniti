270b1d58ba633a23df14361875c43eae1d50c6de
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-06-28 06:21:55 -0400

    combined first and last names

--- client/dashboard/src/js/app.js
+++ client/dashboard/src/js/app.js
@@ -7,10 +7,9 @@ const myQueue = document.getElementById("queueContent");
 myQueue.innerHTML = "";
 myQueue.innerHTML += `<div class="row" style="background-color:black;color:white;width:94vw;margin:0.8rem;margin-top:0;font-size:1.5rem;">`
  + `<div class="col-md-1">ID</div>`
- + `<div class="col-md-2">First</div>`
- + `<div class="col-md-2">Last</div>`
+ + `<div class="col-md-3">Name</div>`
  + `<div class="col-md-1">Year</div>`
- + `<div class="col-md-2">Color</div>`
+ + `<div class="col-md-3">Color</div>`
  + `<div class="col-md-1">Model</div>`
  + `<div class="col-md-2">Price</div>`
  + `</div>`;
@@ -27,10 +26,9 @@ const populateLine = (ticket) => {
   myQueue.appendChild(div);
   const myDiv = document.getElementById(lineID);
   myDiv.innerHTML = `<div class="col-md-1">${ticket.saleRequestID}</div>`
-   + `<div class="col-md-2">${ticket.client.firstName}</div>`
-   + `<div class="col-md-2">${ticket.client.lastName}</div>`
+   + `<div class="col-md-3">${ticket.client.firstName} ${ticket.client.lastName}</div>`
    + `<div class="col-md-1">${ticket.vehicle.year}</div>`
-   + `<div class="col-md-2">${ticket.vehicle.color}</div>`
+   + `<div class="col-md-3">${ticket.vehicle.color}</div>`
    + `<div class="col-md-1">${ticket.vehicle.model}</div>`
    + `<div class="col-md-2">\$${numberWithCommas(ticket.vehicle.retailPrice)}</div>`;
 
