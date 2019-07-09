76ed063ac4748990484dd16e145993d506abc38c
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-07-09 13:19:28 -0400

    Added some persistent checking of correct queue with javascript and localStorage

--- client/dashboard/src/js/login.js
+++ client/dashboard/src/js/login.js
@@ -1,27 +1,31 @@
+if (localStorage.getItem("myURL")) {
+  localStorage.removeItem("myURL");
+}
 
 const loginValues = [{
   user: 'finance@bi.com',
   password: 'letmein',
-  url: './finance.html'
+  url: 'http://localhost:5500/client/dashboard/src/finance.html'
 }, {
   user: 'lease@bi.com',
   password: 'letmein',
-  url: './lease.html'
+  url: 'http://localhost:5500/client/dashboard/src/lease.html'
 }, {
   user: 'fullsale@bi.com',
   password: 'letmein',
-  url: './full-sale.html'
+  url: 'http://localhost:5500/client/dashboard/src/full-sale.html'
 }];
 
 const $user = $('#inputEmail');
 const $password = $('#inputPassword');
-$('#login').click(function(e) {
+$('#login').click(function (e) {
   e.preventDefault();
 
   let url = './index.html';
-  $.each(loginValues, function(i, loginValue) {
+  $.each(loginValues, function (i, loginValue) {
     if ($user.val() === loginValue.user && $password.val() === loginValue.password) {
       url = loginValue.url;
+      localStorage.setItem("myURL", loginValue.url);
       return false;
     }
   });
